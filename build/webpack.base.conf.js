9920bef83acdecb3896dcbac76b7d2b6941841b6
chengzhang <676931854@qq.com>
2018-03-27 10:11:32 +0800

    faefg

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -32,11 +32,7 @@ module.exports = {
         loader: 'vue-loader',
         options: vueLoaderConfig
       },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
-      },
+      
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
